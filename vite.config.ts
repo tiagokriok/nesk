be13c731e4802ce9f19ef347cfb6dc7443be822f
Tiago Carioca <tiagoacarioca@gmail.com>
2022-11-27 21:19:05 -0400

    chore: add icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { rmSync } from 'fs'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import pkg from './package.json'
+import vue from '@vitejs/plugin-vue';
+import { rmSync } from 'fs';
+import { defineConfig } from 'vite';
+import electron from 'vite-plugin-electron';
+import renderer from 'vite-plugin-electron-renderer';
+import pkg from './package.json';
 
-rmSync('dist-electron', { recursive: true, force: true })
-const sourcemap = !!process.env.VSCODE_DEBUG
-const isBuild = process.argv.slice(2).includes('build')
+rmSync('dist-electron', { recursive: true, force: true });
+const sourcemap = !!process.env.VSCODE_DEBUG;
+const isBuild = process.argv.slice(2).includes('build');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +19,11 @@ export default defineConfig({
         entry: 'electron/main/index.ts',
         onstart(options) {
           if (process.env.VSCODE_DEBUG) {
-            console.log(/* For `.vscode/.debug.script.mjs` */'[startup] Electron App')
+            console.log(
+              /* For `.vscode/.debug.script.mjs` */ '[startup] Electron App'
+            );
           } else {
-            options.startup()
+            options.startup();
           }
         },
         vite: {
@@ -38,9 +40,9 @@ export default defineConfig({
       {
         entry: 'electron/preload/index.ts',
         onstart(options) {
-          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
           // instead of restarting the entire Electron App.
-          options.reload()
+          options.reload();
         },
         vite: {
           build: {
@@ -52,19 +54,21 @@ export default defineConfig({
             },
           },
         },
-      }
+      },
     ]),
     // Use Node.js API in the Renderer-process
     renderer({
       nodeIntegration: true,
     }),
   ],
-  server: process.env.VSCODE_DEBUG ? (() => {
-    const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-    return {
-      host: url.hostname,
-      port: +url.port,
-    }
-  })() : undefined,
+  server: process.env.VSCODE_DEBUG
+    ? (() => {
+        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+        return {
+          host: url.hostname,
+          port: +url.port,
+        };
+      })()
+    : undefined,
   clearScreen: false,
-})
+});
